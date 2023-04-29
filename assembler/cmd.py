9c1f6f163ac08daf44e9890356431b0f8a174a1e
linguini1 <matteo.golin@gmail.com>
2023-04-29 12:48:54 -0400

    Added commandline arguments for providing in and out files to the assembler

---
+++ assembler/cmd.py
@@ -0,0 +1,61 @@
+# The commandline argument parser for the prototype assembler
+__author__ = "Matteo Golin"
+
+# Imports
+import argparse
+from pathlib import Path
+
+# Constants
+ASM_FILE_EXT: str = ".orgasm"
+OBJ_FILE_EXT: str = ".obj"
+DESCRIPTION: str = "A commandline tool for assembling gol-16 assembly language into binary."
+
+
+# Custom types
+def ORGASM(_parser: argparse.ArgumentParser, filepath: str) -> Path:
+    """Returns the filepath if it is a valid 'original gol-16 asm' file that exists."""
+
+    path = Path(filepath)
+
+    if not (path.exists() and path.is_file()):
+        _parser.error(f"{filepath} not found.")
+
+    if not path.name.endswith(ASM_FILE_EXT):
+        _parser.error(f"{filepath} not {ASM_FILE_EXT} format.")
+
+    return path
+
+
+def OBJFile(_parser: argparse.ArgumentParser, filepath: str) -> Path:
+    """Ensures that the provided filepath is a valid .obj filepath."""
+
+    path = Path(filepath)
+
+    if not path.name.endswith(OBJ_FILE_EXT):
+        parser.error(f"{filepath} is not a {OBJ_FILE_EXT} file.")
+
+    if not path.parent.exists():
+        parser.error(f"{filepath} parent directory does not exist.")
+
+    return path
+
+
+# Parser
+parser = argparse.ArgumentParser(
+    description=DESCRIPTION
+)
+
+# Commands
+parser.add_argument(
+    "i",
+    help="The input gol-16 asm file.",
+    nargs=1,
+    type=lambda x: ORGASM(parser, x),
+)
+
+parser.add_argument(
+    "-o",
+    nargs=1,
+    type=lambda x: OBJFile(parser, x),
+    default=None  # Default to same name and location as input file .obj
+)
