acfa33ac031f3832c792b0f16af322a24c174cf9
Matteo Golin <matteo.golin@gmail.com>
2023-06-29 15:33:40 -0400

    Added conversion for Form3 directives.

--- assembler/src/conversion/analyzer.c
+++ assembler/src/conversion/analyzer.c
@@ -1,4 +1,5 @@
 #include "analyzer.h"
+#include "identifiers.h"
 #include "tokens.h"
 #include <stdint.h>
 #include <stdio.h>
@@ -69,14 +70,35 @@ static uint16_t _analyzer_convert_statement(Analyzer *analyzer, char **err_msg) 
         return _analyzer_convert_form1(analyzer, operator->raw, err_msg);
     case Form2:
         return _analyzer_convert_form2(analyzer, operator->raw, err_msg);
+    case Form3:
+        return _analyzer_convert_form3(analyzer, operator->raw, err_msg);
     default:
         *err_msg = "Unrecognized operator.";
         return 0;
     }
 }
 
-static uint16_t _analyzer_convert_form1(Analyzer *analyzer, const unsigned short int opcodes[], char **err_msg) {
+static uint16_t _analyzer_convert_dcd(Analyzer *analyzer, char **err_msg) {
+    _analyzer_read_token(analyzer);
+    switch (analyzer->token->type) {
+    case TokenBin:
+        return strtol(analyzer->token->literal, NULL, 2);
+    case TokenHex:
+        return strtol(analyzer->token->literal, NULL, 16);
+    case TokenDec:
+        return atoi(analyzer->token->literal);
+    case TokenChar:
+        return analyzer->token->literal[0];
+    case TokenStr:
+        analyzer->__str_in_prog = analyzer->token->literal;
+        return _str_literal(analyzer);
+    default:
+        *err_msg = "Expected literal to follow DCD.";
+        return 0;
+    }
+}
 
+static uint16_t _analyzer_convert_form1(Analyzer *analyzer, const unsigned short int opcodes[], char **err_msg) {
     uint16_t inst = 0;
 
     // Next argument must be a register
@@ -140,7 +162,7 @@ static uint16_t _analyzer_convert_form1(Analyzer *analyzer, const unsigned short
     return inst;
 }
 
-static uint16_t _analyzer_convert_form2(Analyzer *analyzer, const unsigned short int opcodes[], char **err_msg){
+static uint16_t _analyzer_convert_form2(Analyzer *analyzer, const unsigned short int opcodes[], char **err_msg) {
 
     uint16_t inst = 0;
 
@@ -180,6 +202,15 @@ static uint16_t _analyzer_convert_form2(Analyzer *analyzer, const unsigned short
         inst = inst | (opcodes[1] << 11);
         inst = inst | (atoi(analyzer->token->literal) & 0x1FF);
         break;
+    case TokenIdentifier:
+        inst = inst | (opcodes[1] << 11);
+        ident_t *ident = lookup_tree_get(analyzer->lookup_tree, analyzer->token->literal);
+        if (ident == NULL) {
+            *err_msg = "Undefined indentifier.";
+            return 0;
+        }
+        inst = inst | (ident->location & 0x1FF);
+        break;
     default:
         *err_msg = "Expected numerical immediate or register.";
         return 0;
@@ -188,24 +219,140 @@ static uint16_t _analyzer_convert_form2(Analyzer *analyzer, const unsigned short
     return inst;
 }
 
-static uint16_t _analyzer_convert_dcd(Analyzer *analyzer, char **err_msg) {
+static uint16_t _analyzer_convert_form3(Analyzer *analyzer, const unsigned short int opcodes[], char **err_msg) {
+
+    uint16_t inst = 0;
+
+    // Next argument must be a register
     _analyzer_read_token(analyzer);
-    switch (analyzer->token->type) {
-    case TokenBin:
-        return strtol(analyzer->token->literal, NULL, 2);
-    case TokenHex:
-        return strtol(analyzer->token->literal, NULL, 16);
-    case TokenDec:
-        return atoi(analyzer->token->literal);
-    case TokenChar:
-        return analyzer->token->literal[0];
-    case TokenStr:
-        analyzer->__str_in_prog = analyzer->token->literal;
-        return _str_literal(analyzer);
-    default:
-        *err_msg = "Expected literal to follow DCD.";
+    if (analyzer->token->type != TokenRegister) {
+        *err_msg = "Expected register.";
         return 0;
     }
+
+    inst = inst | _convert_register(analyzer->token->literal);
+    inst = inst << 2;
+
+    // Next token must be a comma
+    _analyzer_read_token(analyzer);
+    if (analyzer->token->type != TokenComma) {
+        *err_msg = "Expected comma.";
+        return 0;
+    }
+
+    // Next token must be open [
+    _analyzer_read_token(analyzer);
+    if (analyzer->token->type != TokenLBrack) {
+        *err_msg = "Expected open bracket ([).";
+        return 0;
+    }
+
+    // Next token can either be a register, identifier or immediate
+    _analyzer_read_token(analyzer);
+    uint16_t immediate;
+    bool imm = true;
+    switch (analyzer->token->type) {
+    case TokenHex:
+        immediate = (strtol(analyzer->token->literal, NULL, 16) & 0x1FF);
+        break;
+    case TokenBin:
+        immediate = (strtol(analyzer->token->literal, NULL, 2) & 0x1FF);
+        break;
+    case TokenDec:
+        immediate = (atoi(analyzer->token->literal) & 0x1FF);
+        break;
+    case TokenIdentifier: {
+        ident_t *ident = lookup_tree_get(analyzer->lookup_tree, analyzer->token->literal);
+        if (ident == NULL) {
+            *err_msg = "Undefined identifier.";
+            return 0;
+        }
+        immediate = ident->location & 0x1FF;
+        break;
+    }
+    case TokenRegister:
+        imm = false;
+        inst = inst | _convert_register(analyzer->token->literal);
+        inst = inst << 2;
+        break;
+    default:
+        *err_msg = "Expected register, identifier or numerical immediate.";
+        return 0;
+    }
+
+    // Argument was an immediate
+    if (imm) {
+        inst = inst << 9;                  // Move over register bits
+        inst = inst | (opcodes[0] << 11);  // Add the opcode
+        inst = inst | (immediate & 0x1FF); // Add the immediate to the end
+
+        // Check that instruction closes with ]
+        _analyzer_read_token(analyzer);
+        if (analyzer->token->type != TokenRBrack) {
+            *err_msg = "Expected closing bracket.";
+            return 0;
+        }
+
+        return inst;
+    }
+
+    // By now, instruction is either [register, register] or [register, imm7]
+
+    // Next token must be a comma
+    _analyzer_read_token(analyzer);
+    if (analyzer->token->type != TokenComma) {
+        *err_msg = "Expected comma.";
+        return 0;
+    }
+
+    // Next token is either a register or an immediate/identifier
+    _analyzer_read_token(analyzer);
+    imm = true;
+    switch (analyzer->token->type) {
+    case TokenHex:
+        inst = inst << 9;
+        immediate = (strtol(analyzer->token->literal, NULL, 16) & 0x1FF);
+        break;
+    case TokenBin:
+        inst = inst << 9;
+        immediate = (strtol(analyzer->token->literal, NULL, 2) & 0x1FF);
+        break;
+    case TokenDec:
+        inst = inst << 9;
+        immediate = (atoi(analyzer->token->literal) & 0x1FF);
+        break;
+    case TokenIdentifier: {
+        inst = inst << 9;
+        ident_t *ident = lookup_tree_get(analyzer->lookup_tree, analyzer->token->literal);
+        if (ident == NULL) {
+            *err_msg = "Undefined identifier.";
+            return 0;
+        }
+        immediate = ident->location & 0x1FF;
+        break;
+    }
+    case TokenRegister:
+        imm = false;
+        inst = inst | _convert_register(analyzer->token->literal);
+        inst = inst << 5;
+        break;
+    default:
+        *err_msg = "Expected register, identifier or numerical immediate.";
+        return 0;
+    }
+
+    // Check that instruction closes with ]
+    _analyzer_read_token(analyzer);
+    if (analyzer->token->type != TokenRBrack) {
+        *err_msg = "Expected closing bracket.";
+        return 0;
+    }
+
+    if (imm) {
+        return inst | (opcodes[2] << 11);
+    }
+
+    return inst | (opcodes[1] << 11);
 }
 
 /* Operator identification */
