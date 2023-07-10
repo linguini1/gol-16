d32b4b0bb54c57ebc60563b933aebc0064aa9a73
Matteo Golin <matteo.golin@gmail.com>
2023-07-10 16:45:19 -0400

    Fixed static declarations in Lexer

--- assembler/src/conversion/lexer.c
+++ assembler/src/conversion/lexer.c
@@ -19,32 +19,76 @@ bool _is_orgasm_file(const char *filename) {
     return !strcmp(cur, FILE_SUFFIX);
 }
 
-/* Lexer */
-Lexer *lexer_construct(const char *file_path) {
-
-    // Verify compatible file
-    if (!_is_orgasm_file(file_path)) {
-        return NULL;
-    }
-
-    // Verify file could be opened
-    FILE *fptr = fopen(file_path, "rb");
-    if (fptr == NULL) {
-        return NULL;
-    }
-
-    Lexer *lexer = malloc(sizeof(Lexer));
-    lexer->file_path = file_path;
-    lexer->stream = fptr;
-    lexer->line = 1;
-    lexer->col = 1;
-    _lexer_read_char(lexer);
-    return lexer;
+static char *_struprcpy(char *ident) {
+    size_t length = strlen(ident);
+    char *upr_ident = malloc(length + 1);
+    strcpy(upr_ident, ident);
+    string_to_uppercase(upr_ident);
+    return upr_ident;
 }
 
-void lexer_destruct(Lexer *lexer) {
-    fclose(lexer->stream);
-    free(lexer);
+/* Character classification */
+static bool is_letter(char c) { return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'); }
+static bool is_num(char c) { return '0' <= c && c <= '9'; }
+static bool is_bin(char c) { return c == '0' || c == '1'; }
+static bool is_hex(char c) { return is_num(c) || ('A' <= c && c <= 'F') || ('a' <= c && c <= 'f'); }
+static bool is_whitespace(char c) { return c == ' ' || c == '\n' || c == '\t' || c == '\r'; }
+
+static bool is_register(char *ident) {
+
+    if (ident == NULL) {
+        return false;
+    }
+
+    char *upr_ident = _struprcpy(ident);
+
+    bool reg =
+        !strcmp(upr_ident, "R0") || !strcmp(upr_ident, "R1") || !strcmp(upr_ident, "R2") || !strcmp(upr_ident, "R3");
+    free(upr_ident);
+    return reg;
+}
+
+static bool is_special_register(char *ident) {
+    if (ident == NULL) {
+        return false;
+    }
+
+    char *upr_ident = _struprcpy(ident);
+
+    bool spec =
+        !strcmp(upr_ident, "SP") || !strcmp(upr_ident, "PC") || !strcmp(upr_ident, "LR") || !strcmp(upr_ident, "FR");
+
+    free(upr_ident);
+    return spec;
+}
+
+static bool is_operator(char *ident) {
+
+    if (ident == NULL) {
+        return false;
+    }
+
+    // Create uppercase copy of identifier
+    size_t length = strlen(ident);
+    char *upr_ident = malloc(length + 1);
+    strcpy(upr_ident, ident);
+    string_to_uppercase(upr_ident);
+
+    if (length == 0) {
+        free(upr_ident);
+        return false;
+    }
+
+    // Check unconditional operators
+    for (size_t i = 0; i < NUM_OPERATORS; i++) {
+        if (!strcmp(upr_ident, OPERATORS[i].name)) {
+            free(upr_ident);
+            return true;
+        }
+    }
+
+    free(upr_ident);
+    return is_conditional(ident);
 }
 
 bool lexer_eof(Lexer *lexer) { return lexer->character == EOF; }
@@ -56,64 +100,6 @@ static void lexer_fatal_error(Lexer *lexer, const char *err_msg) {
     }
     printf(format_string, lexer->file_path, lexer->line, lexer->col, err_msg, lexer->character);
     exit(EXIT_FAILURE);
-}
-
-Token *lexer_next_token(Lexer *lexer) {
-
-    // Skip white space and comments until we reach something
-    while (is_whitespace(lexer->character) || lexer->character == ';') {
-        _lexer_skip_whitespace(lexer);
-        _lexer_skip_comment(lexer);
-    }
-
-    switch (lexer->character) {
-    case ',':
-        _lexer_read_char(lexer);
-        return token_construct(",", TokenComma, lexer->line, lexer->col);
-    case '[':
-        _lexer_read_char(lexer);
-        return token_construct("[", TokenLBrack, lexer->line, lexer->col);
-    case ']':
-        _lexer_read_char(lexer);
-        return token_construct("]", TokenRBrack, lexer->line, lexer->col);
-    case '{':
-        _lexer_read_char(lexer);
-        return token_construct("{", TokenLCurl, lexer->line, lexer->col);
-    case '}':
-        _lexer_read_char(lexer);
-        return token_construct("}", TokenRCurl, lexer->line, lexer->col);
-    case -1:
-        return token_construct(NULL, TokenEOF, lexer->line, lexer->col);
-    case '"':
-        return token_construct(_lexer_read_string_literal(lexer), TokenStr, lexer->line, lexer->col);
-    case '\'':
-        return token_construct(_lexer_read_char_literal(lexer), TokenChar, lexer->line, lexer->col);
-    case '#': {
-        token_t num_type = TokenIllegal; // Illegal by default until set
-        char *literal = _lexer_read_numeric_literal(lexer, &num_type);
-        return token_construct(literal, num_type, lexer->line, lexer->col);
-    }
-    }
-
-    if (is_letter(lexer->character) || lexer->character == '_') {
-        char *identifier = _lexer_read_identifier(lexer);
-        token_t ident_type = TokenIdentifier;
-
-        if (is_operator(identifier)) {
-            ident_type = TokenOperator;
-            string_to_uppercase(identifier);
-        } else if (is_register(identifier)) {
-            ident_type = TokenRegister;
-            string_to_uppercase(identifier);
-        } else if (is_special_register(identifier)) {
-            ident_type = TokenSpecialRegister;
-            string_to_uppercase(identifier);
-        }
-        return token_construct(identifier, ident_type, lexer->line, lexer->col);
-    }
-
-    lexer_fatal_error(lexer, "Illegal token.");
-    return NULL;
 }
 
 /* Helper internals */
@@ -265,74 +251,88 @@ static char *_lexer_read_char_literal(Lexer *lexer) {
     return value;
 }
 
-/* Character classification */
-static bool is_letter(char c) { return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'); }
-static bool is_num(char c) { return '0' <= c && c <= '9'; }
-static bool is_bin(char c) { return c == '0' || c == '1'; }
-static bool is_hex(char c) { return is_num(c) || ('A' <= c && c <= 'F') || ('a' <= c && c <= 'f'); }
-static bool is_whitespace(char c) { return c == ' ' || c == '\n' || c == '\t' || c == '\r'; }
+/* Lexer */
+Lexer *lexer_construct(const char *file_path) {
 
-static char *_struprcpy(char *ident) {
-    size_t length = strlen(ident);
-    char *upr_ident = malloc(length + 1);
-    strcpy(upr_ident, ident);
-    string_to_uppercase(upr_ident);
-    return upr_ident;
+    // Verify compatible file
+    if (!_is_orgasm_file(file_path)) {
+        return NULL;
+    }
+
+    // Verify file could be opened
+    FILE *fptr = fopen(file_path, "rb");
+    if (fptr == NULL) {
+        return NULL;
+    }
+
+    Lexer *lexer = malloc(sizeof(Lexer));
+    lexer->file_path = file_path;
+    lexer->stream = fptr;
+    lexer->line = 1;
+    lexer->col = 1;
+    _lexer_read_char(lexer);
+    return lexer;
 }
 
-static bool is_register(char *ident) {
-
-    if (ident == NULL) {
-        return false;
-    }
-
-    char *upr_ident = _struprcpy(ident);
-
-    bool reg =
-        !strcmp(upr_ident, "R0") || !strcmp(upr_ident, "R1") || !strcmp(upr_ident, "R2") || !strcmp(upr_ident, "R3");
-    free(upr_ident);
-    return reg;
+void lexer_destruct(Lexer *lexer) {
+    fclose(lexer->stream);
+    free(lexer);
 }
 
-static bool is_special_register(char *ident) {
-    if (ident == NULL) {
-        return false;
+Token *lexer_next_token(Lexer *lexer) {
+
+    // Skip white space and comments until we reach something
+    while (is_whitespace(lexer->character) || lexer->character == ';') {
+        _lexer_skip_whitespace(lexer);
+        _lexer_skip_comment(lexer);
     }
 
-    char *upr_ident = _struprcpy(ident);
-
-    bool spec =
-        !strcmp(upr_ident, "SP") || !strcmp(upr_ident, "PC") || !strcmp(upr_ident, "LR") || !strcmp(upr_ident, "FR");
-
-    free(upr_ident);
-    return spec;
-}
-
-static bool is_operator(char *ident) {
-
-    if (ident == NULL) {
-        return false;
+    switch (lexer->character) {
+    case ',':
+        _lexer_read_char(lexer);
+        return token_construct(",", TokenComma, lexer->line, lexer->col);
+    case '[':
+        _lexer_read_char(lexer);
+        return token_construct("[", TokenLBrack, lexer->line, lexer->col);
+    case ']':
+        _lexer_read_char(lexer);
+        return token_construct("]", TokenRBrack, lexer->line, lexer->col);
+    case '{':
+        _lexer_read_char(lexer);
+        return token_construct("{", TokenLCurl, lexer->line, lexer->col);
+    case '}':
+        _lexer_read_char(lexer);
+        return token_construct("}", TokenRCurl, lexer->line, lexer->col);
+    case -1:
+        return token_construct(NULL, TokenEOF, lexer->line, lexer->col);
+    case '"':
+        return token_construct(_lexer_read_string_literal(lexer), TokenStr, lexer->line, lexer->col);
+    case '\'':
+        return token_construct(_lexer_read_char_literal(lexer), TokenChar, lexer->line, lexer->col);
+    case '#': {
+        token_t num_type = TokenIllegal; // Illegal by default until set
+        char *literal = _lexer_read_numeric_literal(lexer, &num_type);
+        return token_construct(literal, num_type, lexer->line, lexer->col);
+    }
     }
 
-    // Create uppercase copy of identifier
-    size_t length = strlen(ident);
-    char *upr_ident = malloc(length + 1);
-    strcpy(upr_ident, ident);
-    string_to_uppercase(upr_ident);
+    if (is_letter(lexer->character) || lexer->character == '_') {
+        char *identifier = _lexer_read_identifier(lexer);
+        token_t ident_type = TokenIdentifier;
 
-    if (length == 0) {
-        free(upr_ident);
-        return false;
-    }
-
-    // Check unconditional operators
-    for (size_t i = 0; i < NUM_OPERATORS; i++) {
-        if (!strcmp(upr_ident, OPERATORS[i].name)) {
-            free(upr_ident);
-            return true;
+        if (is_operator(identifier)) {
+            ident_type = TokenOperator;
+            string_to_uppercase(identifier);
+        } else if (is_register(identifier)) {
+            ident_type = TokenRegister;
+            string_to_uppercase(identifier);
+        } else if (is_special_register(identifier)) {
+            ident_type = TokenSpecialRegister;
+            string_to_uppercase(identifier);
         }
+        return token_construct(identifier, ident_type, lexer->line, lexer->col);
     }
 
-    free(upr_ident);
-    return is_conditional(ident);
+    lexer_fatal_error(lexer, "Illegal token.");
+    return NULL;
 }
--- assembler/src/conversion/lexer.h
+++ assembler/src/conversion/lexer.h
@@ -7,9 +7,6 @@
 
 static const char *FILE_SUFFIX = ".orgasm";
 
-/* File type verification */
-static bool _is_orgasm_file(const char *filename);
-
 /* Lexer */
 typedef struct Lexer {
     FILE *stream;
@@ -23,38 +20,6 @@ Lexer *lexer_construct(const char *file_path);
 void lexer_destruct(Lexer *lexer);
 
 bool lexer_eof(Lexer *lexer);
-static void lexer_fatal_error(Lexer *lexer, const char *err_msg);
 Token *lexer_next_token(Lexer *lexer);
-
-/* Character classification */
-static char *_struprcpy(char *ident);
-static bool is_letter(char c);
-static bool is_num(char c);
-static bool is_whitespace(char c);
-static bool is_bin(char c);
-static bool is_hex(char c);
-static bool is_operator(char *ident);
-static bool is_register(char *ident);
-static bool is_special_register(char *ident);
-
-/* Lexer helper internals */
-static char *_lexer_slice(Lexer *lexer, long start);
-static char _lexer_peek(Lexer *lexer);
-static void _lexer_read_char(Lexer *lexer);
-
-/* Lexer skipping */
-static void _lexer_skip_whitespace(Lexer *lexer);
-static void _lexer_skip_comment(Lexer *lexer);
-
-/* Lexer reading tokens */
-static char *_lexer_read_identifier(Lexer *lexer);
-
-static char *_lexer_read_numeric_literal(Lexer *lexer, token_t *type);
-static char *_lexer_read_decimal_literal(Lexer *lexer);
-static char *_lexer_read_hex_literal(Lexer *lexer);
-static char *_lexer_read_bin_literal(Lexer *lexer);
-
-static char *_lexer_read_string_literal(Lexer *lexer);
-static char *_lexer_read_char_literal(Lexer *lexer);
 
 #endif // _LEXER_H_
